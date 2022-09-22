0820161f7951f31d230a542950a9c6b80cf453ca
qotsa7 <mtranqua@gmail.fr>
2022-09-22 14:33:59 +0200

    fix position ping notif

--- src/component/BellNotification.jsx
+++ src/component/BellNotification.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const BellNotification = () => {
     return <>
-        <span class="flex h-2 w-2 relative -top-1 right-6 z-10">
+        <span class="flex h-2 w-2 absolute top-2 left-8 z-10">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
             <span class="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
         </span>
