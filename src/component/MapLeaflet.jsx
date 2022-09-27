c29bab03655bdd69815dc4d56e4c6eb8624af5d2
qotsa7 <mtranqua@gmail.fr>
2022-09-27 09:34:24 +0200

    ajout icon afpa sur la map

--- src/component/MapLeaflet.jsx
+++ src/component/MapLeaflet.jsx
@@ -2,33 +2,17 @@ import { useEffect } from "react";
 import "../../node_modules/leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import LogoAfpa from "../assets/LogoAfpa.jpg";
 
-delete L.Icon.Default.prototype._getIconUrl;
-
-console.log({LogoAfpa});
-
-// TODO: taille icones
-L.Icon.Default.mergeOptions({
-    // iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    // iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    // shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-    iconRetinaUrl: require(`../assets/LogoAfpa.jpg`),
-    iconUrl: require(`../assets/LogoAfpa.jpg`)
+const afpaIcon = new L.Icon({
+    iconUrl: require(`../assets/LogoAfpa.jpg`),
+    iconSize: [60, 30],
+    shadowSize: [0, 0],
+    className: "object-fill border-solid border border-black"
 });
 
 const MapLeaflet = () => {
 
-    // const [latitude, setLatitude] = useState(null);
-    // const [longitude, setLongitude] = useState(null);
-
-    // useEffect(() => {
-    //     setLatitude(45.95864);
-    //     setLongitude(-0.96423);
-    // }, [])
-
-
-    const afpaPos = [45.95864, -0.96423];
+    const afpaPos = [45.95810, -0.96423];
 
     useEffect(() => {
     }, [])
@@ -39,7 +23,7 @@ const MapLeaflet = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={afpaPos}>
+            <Marker position={afpaPos} icon={afpaIcon} >
                 <Popup>
                     AFPA Rochefort
                 </Popup>
