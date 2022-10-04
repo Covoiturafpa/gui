027e8269e3f03dad0e854995349b06792e1a81ec
qotsa7 <mtranqua@gmail.fr>
2022-10-04 10:00:44 +0200

    mapleaflet v1

--- src/component/MapLeaflet.jsx
+++ src/component/MapLeaflet.jsx
@@ -5,7 +5,6 @@ import { MapContainer, TileLayer, Marker, Popup, Icon, useMap } from 'react-leaf
 import L from 'leaflet';
 import "leaflet-routing-machine";
 import { DestinationContext as DestinationContext } from "../scenes/BookingForm";
-import authHeader from '../services/AuthHeader';
 import FetchService from "../services/FetchService";
 
 const afpaIcon = new L.Icon({
@@ -17,13 +16,9 @@ const afpaIcon = new L.Icon({
 
 const MapLeaflet = () => {
 
-
-
     const { destination } = useContext(DestinationContext);
     const [waypoints, setWaypoints] = useState({});
-    const [afpaPos, setAfpaPos] = useState([0, 0])
-    // [45.95810, -0.96423];
-
+    const [afpaPos, setAfpaPos] = useState([])
 
     delete L.Icon.Default.prototype._getIconUrl;
     L.Icon.Default.mergeOptions({
@@ -34,13 +29,12 @@ const MapLeaflet = () => {
 
     useEffect(() => {
         FetchService.get("/centre").then((data) => {
-            const centreInformations = data;
-            setAfpaPos([centreInformations.latitude, centreInformations.longitude]);
+            const coordinates = [data.latitude, data.longitude];
+            setAfpaPos(coordinates);
         })
     }, [])
 
     useEffect(() => {
-
         setWaypoints({
             destination: {
                 lat: destination.lat,
@@ -51,23 +45,23 @@ const MapLeaflet = () => {
                 lon: afpaPos[1]
             }
         })
-        //setWaypoints([destination.lat, destination.lon, afpaPos[0], afpaPos[1]]);
-
     }, [destination])
 
     return (
-        <MapContainer center={afpaPos} zoom={12} scrollWheelZoom={true} className='h-full w-full'>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={afpaPos} icon={afpaIcon} >
-                <Popup>
-                    AFPA Rochefort
-                </Popup>
-            </Marker>
-            <Routing waypoints={waypoints} />
-        </MapContainer>
+        afpaPos.length != 0 ?
+            <MapContainer center={afpaPos} zoom={12} scrollWheelZoom={true} className='h-full w-full'>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={afpaPos} icon={afpaIcon} >
+                    <Popup>
+                        AFPA Rochefort
+                    </Popup>
+                </Marker>
+                <Routing waypoints={waypoints} />
+            </MapContainer>
+            : null
     )
 }
 
