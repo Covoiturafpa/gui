c6bf3bf32bf9bddf303224b28d26342402cc8c2a
Juju-D <72973194+Juju-D@users.noreply.github.com>
2022-10-14 12:00:27 +0200

    add ride form json

--- src/component/RideForms/AddRideForm.jsx
+++ src/component/RideForms/AddRideForm.jsx
@@ -63,7 +63,7 @@ const AddRideForm = (props) => {
                 "longitude" : destination.value.lon,
                 "isFromAfpa" : isFromAfpa.value,
                 "city" : {
-                    "name" : (isFromAfpa ? arrival.value : departure.value)
+                    "name" : (isFromAfpa.value ? arrival.value : departure.value)
                 }
             },
             "departureTime" : arrivalTime.toISOString().substring(11, 19),
@@ -97,7 +97,7 @@ const AddRideForm = (props) => {
                                         "longitude" : destination.value.lon,
                                         "isFromAfpa" : !isFromAfpa.value,
                                         "city" : {
-                                            "name" : (isFromAfpa ? arrival.value : departure.value)
+                                            "name" : (isFromAfpa.value ? arrival.value : departure.value)
                                         }
                                     },
                                     "departureTime" : arrivalTimeReturn.toISOString().substring(11, 19)};
