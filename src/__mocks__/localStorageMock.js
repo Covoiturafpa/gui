8fcb61ed3e81fd243e826feebe4c458b28ef97d7
Morgan <tranquard.morgan@gmail.com>
2022-10-27 17:07:52 +0200

    mock localstorage (path du mock à changer)

---
+++ src/__mocks__/localStorageMock.js
@@ -0,0 +1,20 @@
+console.log("hello from localstorage");
+
+var localStorageMock = (function() {
+    var store = {};
+    return {
+      getItem: function(key) {
+        return store[key];
+      },
+      setItem: function(key, value) {
+        store[key] = value.toString();
+      },
+      clear: function() {
+        store = {};
+      },
+      removeItem: function(key) {
+        delete store[key];
+      }
+    };
+  })();
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock });
--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,16 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import AuthService from "../services/AuthService";
 
 const userLogin = {
-    username : "MohammadGreenfelder@mail.fr",
-    password : "6sjngbrc3t1tigidn826"
+    username: "MohammadGreenfelder@mail.fr",
+    password: "6sjngbrc3t1tigidn826"
 }
-  
-test('log in', () => {
-    let result = AuthService.login(userLogin.username, userLogin.password);
-    let id = result.then((res) => res.userId);
-    expect(id).toBe(51);
+
+test('log in', async () => {
+    window.localStorage.clear();
+    let result = await AuthService.login(userLogin.username, userLogin.password);
+    console.log(result)
+    expect(result.id).toBe("51")
 });
 
 test('basic', () => {
     expect(1).toBe(1);
 });
+
