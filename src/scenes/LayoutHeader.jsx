b838af1add0f9092fb8beb98fff86987abb085cc
Juju-D <72973194+Juju-D@users.noreply.github.com>
2022-11-04 11:06:09 +0100

    correction lors de la redirection vers accueil au click sur le logo

--- src/scenes/LayoutHeader.jsx
+++ src/scenes/LayoutHeader.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Header } from 'rsuite';
 import logo from "../assets/logo_covoiturafpa.png";
 
 const LayoutHeader = () => {
+    let navigate = useNavigate();
+
+    const navigateToHome = () => {
+        navigate("/accueil");
+    }
+
     return (<Header className='h-min flex justify-center'>
-        <a className="no-underline hover:no-underline text-gray-700 m-1" href="/accueil">
+        <button className=" text-gray-700 m-1" onClick={navigateToHome}>
             <img className="w-96 object-contain" src={logo} alt="Logo covoiturafpa" />
-        </a>
+        </button>
     </Header>);
 }
 
