62f47a761d8026df6f5632eefd56011f3a652257
JaysonBrement <jayson.brement@outlook.fr>
2025-03-12 12:26:06 +0100

    changement deploiement

--- src/services/FetchService.jsx
+++ src/services/FetchService.jsx
@@ -3,7 +3,7 @@ import authHeader from './AuthHeader'
 class FetchService {
 
     // #urlApi = "https://covoiturafpa-spring-api.herokuapp.com";
-    #urlApi = "http://localhost:8080";
+    #urlApi = "https://localhost:443";
 
     get(endpoint, body) {
         try {
